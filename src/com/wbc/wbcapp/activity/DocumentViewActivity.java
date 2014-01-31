3193b21bf8a0b5be1a51f3e222bdfc9a4249bb9e
Mike Wleklinski <michael@hardindd.com>
2014-01-31 14:45:14 -0600

    tried using webview

--- src/com/wbc/wbcapp/activity/DocumentViewActivity.java
+++ src/com/wbc/wbcapp/activity/DocumentViewActivity.java
@@ -1,33 +1,60 @@
 package com.wbc.wbcapp.activity;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-
-import org.apache.poi.xslf.usermodel.XMLSlideShow;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.Window;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
 
 import com.wbc.wbc.R;
 
 public class DocumentViewActivity extends Activity {
+	
+	WebView documentView; 
+	Activity mActivity;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_document_view);
 		
-		try {
-			XMLSlideShow ppt = new XMLSlideShow(new FileInputStream("slideshow.pptx"));
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		getWindow().requestFeature(Window.FEATURE_PROGRESS);
+		setContentView(R.layout.activity_document_view);
+		mActivity = this;
+		
+		documentView = (WebView) findViewById(R.id.document_view);
+
+		 documentView.getSettings().setJavaScriptEnabled(true);
+
+		 final Activity activity = this;
+		 documentView.setWebChromeClient(new WebChromeClient() {
+		   public void onProgressChanged(WebView view, int progress) {
+		     // Activities and WebViews measure progress with different scales.
+		     // The progress meter will automatically disappear when we reach 100%
+		     activity.setProgress(progress * 1000);
+		   }
+		 });
+		 documentView.setWebViewClient(new WebViewClient() {
+		   public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+		     Toast.makeText(activity, "Oh no! " + description, Toast.LENGTH_SHORT).show();
+		   }
+		 });
+
+		
+		documentView.loadUrl("file:///android_asset/slideshow.htm");
+		
+		
+//		try {
+//			XMLSlideShow ppt = new XMLSlideShow(new FileInputStream("slideshow.pptx"));
+//		} catch (FileNotFoundException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} catch (IOException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
 	}
 
 	@Override
