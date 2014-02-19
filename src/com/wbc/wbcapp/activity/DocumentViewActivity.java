0f3da7180e5a9dd6425b2b020c6f1d248613d034
Mike Wleklinski <michael@hardindd.com>
2014-02-19 16:58:48 -0600

    working with aspose

--- src/com/wbc/wbcapp/activity/DocumentViewActivity.java
+++ src/com/wbc/wbcapp/activity/DocumentViewActivity.java
@@ -9,8 +9,10 @@ import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,8 +20,13 @@ import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 
 import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 
 import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Menu;
@@ -42,6 +49,8 @@ public class DocumentViewActivity extends Activity {
 	WebView documentView;
 	Activity mActivity;
 
+	String appSID, appKey;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -49,151 +58,125 @@ public class DocumentViewActivity extends Activity {
 		getWindow().requestFeature(Window.FEATURE_PROGRESS);
 		setContentView(R.layout.activity_document_view);
 		mActivity = this;
+		documentView = (WebView) findViewById(R.id.document_view);
 
-		AsposeApp.setAppSID("11d55855-f6bd-4b84-8c79-e10be6931d25");
-		AsposeApp.setAppKey("bcec854ec2a184f41483ae0ce68dcde0");
+		appSID = "11d55855-f6bd-4b84-8c79-e10be6931d25";
+		appKey = "bcec854ec2a184f41483ae0ce68dcde0";
+
+		AsposeApp.setAppSID(appSID);
+		AsposeApp.setAppKey(appKey);
+
+		uploadFileToStoarge(appSID, appKey);
 
 		String strURI = "http://api.aspose.com/v1.1/slides/Protection.pptx?format=html";
 		String signedURI = Sign(strURI, "11d55855-f6bd-4b84-8c79-e10be6931d25", "bcec854ec2a184f41483ae0ce68dcde0");
+
+		File document = new File(getFilesDir(), "Protection.htm");
+		document.delete();
+		if (document.exists())
+			documentView.loadUrl("file:///" + document.getAbsolutePath());
+		else
+			Toast.makeText(mActivity, "File not found", Toast.LENGTH_LONG).show();
+
 		InputStream responseStream;
 		try {
-			//responseStream = ProcessCommand(signedURI, "GET");
-			new GetDocumentFromServer().execute(signedURI, "GET", getCacheDir() + "/Protection.html");
-//			File outputPath = new File(getCacheDir() + "/Protection.html");
-//			SaveStreamToFile(outputPath, responseStream);
-//			responseStream.close();
+			new AsyncTask<String, Void, File>() {
+
+				String filename;
+
+				@Override
+				protected File doInBackground(String... args) {
+					InputStream inputStream = null;
+					String strURI = args[0];
+					String strHttpCommand = args[1];
+					File outputPath = new File(args[2], args[3]);
+					filename = args[3];
+					URL address;
+					try {
+						if (isNetworkConnected()) {
+							address = new URL(strURI);
+
+							HttpURLConnection httpCon = (HttpURLConnection) address.openConnection();
+							// httpCon.setDoOutput(true);
+							//
+							// httpCon.setRequestProperty("Content-Type",
+							// "application/json");
+							// httpCon.setRequestProperty("Accept",
+							// "text/json");
+							httpCon.setRequestMethod(strHttpCommand);
+							// if (strHttpCommand.equals("PUT") ||
+							// strHttpCommand.equals("POST"))
+							// httpCon.setFixedLengthStreamingMode(0);
+							// String d = httpCon.getResponseMessage();
+							// System.out.println(d);
+							inputStream = httpCon.getInputStream();
+						}
+					} catch (MalformedURLException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+
+					FileOutputStream outputStream;
+					if (inputStream != null) {
+						try {
+							outputStream = openFileOutput(filename, Context.MODE_PRIVATE);
+							int read = 0;
+							byte[] bytes = new byte[1024];
+
+							while ((read = inputStream.read(bytes)) != -1) {
+								outputStream.write(bytes, 0, read);
+							}
+							outputStream.flush();
+							outputStream.close();
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+
+					// OutputStream outputStream = null;
+					// try {
+					// // read this file into InputStream
+					//
+					// // write the inputStream to a FileOutputStream
+					// outputStream = new FileOutputStream(outputPath);
+					//
+					// int read = 0;
+					// byte[] bytes = new byte[8192];
+					//
+					// while ((read = inputStream.read(bytes)) != -1) {
+					// outputStream.write(bytes, 0, read);
+					// }
+					//
+					// inputStream.close();
+					// outputStream.flush();
+					// outputStream.close();
+					//
+					// } catch (IOException e) {
+					// e.printStackTrace();
+					// }
+					return outputPath;
+				}
+
+				protected void onPostExecute(File file) {
+					if (file.canRead())
+						if (file.exists()) {
+							documentView.loadUrl("file:///" + file.getAbsolutePath());
+							deleteFileFromStorage(filename);
+						}
+				}
+
+			}.execute(signedURI, "GET", getFilesDir().getAbsolutePath(), "Protection.htm");
+
+			// File outputPath = new File(getCacheDir() + "/Protection.html");
+			// SaveStreamToFile(outputPath, responseStream);
+			// responseStream.close();
 		} catch (Exception e2) {
 			// TODO Auto-generated catch block
 			e2.printStackTrace();
 		}
-
-//		try {
-//
-//			AssetManager aMgr = mActivity.getAssets();
-//			InputStream ipStream = aMgr.open("slideshow.pptx");
-//			String filePath = getFilesDir().getAbsolutePath();
-//			InputStream inputStream = null;
-//			OutputStream outputStream = null;
-//
-//			// try {
-//			// // read this file into InputStream
-//			// inputStream = ipStream;
-//			//
-//			// // write the inputStream to a FileOutputStream
-//			// outputStream =
-//			// new FileOutputStream(new
-//			// File("/android-assets/slideshow1.pptx"));
-//			//
-//			// int read = 0;
-//			// byte[] bytes = new byte[1024];
-//			//
-//			// while ((read = inputStream.read(bytes)) != -1) {
-//			// outputStream.write(bytes, 0, read);
-//			// }
-//			//
-//			// System.out.println("Done!");
-//			//
-//			// } catch (IOException e) {
-//			// e.printStackTrace();
-//			// } finally {
-//			// if (inputStream != null) {
-//			// try {
-//			// inputStream.close();
-//			// } catch (IOException e) {
-//			// e.printStackTrace();
-//			// }
-//			// }
-//			// if (outputStream != null) {
-//			// try {
-//			// // outputStream.flush();
-//			// outputStream.close();
-//			// } catch (IOException e) {
-//			// e.printStackTrace();
-//			// }
-//			//
-//			// }
-//			// }
-//
-//			// PresentationMLPackage presentationMLPackage =
-//			// (PresentationMLPackage)OpcPackage.load(new
-//			// java.io.File("assets/slideshow.pptx"));
-//
-//			// int resourceId =
-//			// getResources().getIdentifier("com.your.package:raw/somefile.txt");
-//			// File f = new File(getResources().openRawResource(resourceId));
-//
-//			File f = new File(getCacheDir() + "/slideshow.pptx");
-//			if (!f.exists())
-//				try {
-//
-//					// InputStream is = getAssets().open("slideshow.pptx");
-//
-//					InputStream is = getAssets().open("slideshow.pptx");
-//
-//					// Reader is = new BufferedReader(new InputStreamReader(raw,
-//					// "UTF8"));
-//
-//					int size = is.available();
-//					byte[] buffer = new byte[size];
-//					is.read(buffer);
-//					is.close();
-//
-//					FileOutputStream fos = new FileOutputStream(f);
-//					fos.write(buffer);
-//					fos.close();
-//				} catch (Exception e) {
-//					throw new RuntimeException(e);
-//				}
-//
-//			File inFile = new java.io.File(System.getProperty("user.dir") + "/slideshow.pptx");
-//			boolean okay = inFile.exists();
-//
-//			InputStream is = getAssets().open("slideshow.pptx");
-//			// final LoadFromZipNG loader = new LoadFromZipNG();
-//
-//			// PresentationMLPackage presentationMLPackage =
-//			// (PresentationMLPackage)loader.get(is);
-//			PresentationMLPackage presentationMLPackage = (PresentationMLPackage) PresentationMLPackage
-//					.load(getAssets().open("slideshow.pptx"));
-//			// String inputfilepath = System.getProperty("user.dir") +
-//			// "/sample-docs/pptx/pptx-basic.xml";
-//			// String inputfilepath = System.getProperty("user.dir") +
-//			// "/sample-docs/pptx/lines.pptx";
-//
-//			// Where to save images
-//			SvgExporter.setImageDirPath(getCacheDir() + "/slideshow.xhtml");
-//
-//			// TODO - render slides in document order!
-//			Iterator partIterator = presentationMLPackage.getParts().getParts().entrySet().iterator();
-//			while (partIterator.hasNext()) {
-//
-//				Map.Entry pairs = (Map.Entry) partIterator.next();
-//
-//				Part p = (Part) pairs.getValue();
-//				if (p instanceof SlidePart) {
-//
-//					try {
-//						System.out.println(SvgExporter.svg(presentationMLPackage, (SlidePart) p));
-//					} catch (Exception e) {
-//						// TODO Auto-generated catch block
-//						e.printStackTrace();
-//					}
-//				}
-//			}
-//
-//			// NB: file suffix must end with .xhtml in order to see the SVG in a
-//			// browser
-//		} catch (Docx4JException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		} catch (IOException e1) {
-//			// TODO Auto-generated catch block
-//			e1.printStackTrace();
-//		}
-//
-//		System.out.println("\n\n done .. \n\n");
-
-		documentView = (WebView) findViewById(R.id.document_view);
 
 		documentView.getSettings().setJavaScriptEnabled(true);
 
@@ -213,20 +196,114 @@ public class DocumentViewActivity extends Activity {
 			}
 		});
 
-		documentView.loadUrl(getCacheDir() + "/Protection.html");
+		// documentView.loadUrl(getCacheDir() + "/Protection.pdf");
 		// "file:///android_asset/protection/Protection.html"
-		//getCacheDir() + "/Protection.html"
+		// getCacheDir() + "/Protection.html"
+	}
 
-		// try {
-		// XMLSlideShow ppt = new XMLSlideShow(new
-		// FileInputStream("slideshow.pptx"));
-		// } catch (FileNotFoundException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// } catch (IOException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// }
+	protected void deleteFileFromStorage(final String fileName) {
+
+		new AsyncTask<String, Void, InputStream>() {
+			@Override
+			protected InputStream doInBackground(String... params) {
+				// build URI to delete file from the Aspose cloud storage
+				String strURI = "http://api.aspose.com/v1.1/storage/file/" + fileName;
+
+				// Use the following line to use API version 1.1
+				// String strURI = "http://api.aspose.com/v1.1/storage/file/" +
+				// fileName;
+
+				String signedURI = Sign(strURI, appSID, appKey);
+				InputStream result = null;
+				try {
+					URL address = new URL(signedURI);
+					HttpURLConnection httpCon = (HttpURLConnection) address.openConnection();
+					// httpCon.setDoOutput(true);
+
+					httpCon.setRequestProperty("Content-Type", "application/json");
+					httpCon.setRequestProperty("Accept", "text/json");
+					httpCon.setRequestMethod("DELETE");
+					// if (strHttpCommand.equals("PUT") ||
+					// strHttpCommand.equals("POST"))
+					// httpCon.setFixedLengthStreamingMode(0);
+					String d = httpCon.getResponseMessage();
+					System.out.println(d);
+					result = httpCon.getInputStream();
+				} catch (ProtocolException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				return result;
+			}
+		}.execute();
+//		InputStream responseStream;
+//		try {
+//			responseStream = ProcessCommand(signedURI, "DELETE");
+//			//String strJSON = IOUtils.toString(responseStream);
+//		} catch (Exception e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+
+	}
+
+	public static InputStream ProcessCommand(final String strURI, final String strHttpCommand) throws Exception {
+
+		new AsyncTask<String, Void, InputStream>() {
+			@Override
+			protected InputStream doInBackground(String... params) {
+				InputStream result = null;
+				try {
+					URL address = new URL(strURI);
+					HttpURLConnection httpCon = (HttpURLConnection) address.openConnection();
+					// httpCon.setDoOutput(true);
+
+					httpCon.setRequestProperty("Content-Type", "application/json");
+					httpCon.setRequestProperty("Accept", "text/json");
+					httpCon.setRequestMethod(strHttpCommand);
+					// if (strHttpCommand.equals("PUT") ||
+					// strHttpCommand.equals("POST"))
+					// httpCon.setFixedLengthStreamingMode(0);
+					String d = httpCon.getResponseMessage();
+					System.out.println(d);
+					result = httpCon.getInputStream();
+				} catch (ProtocolException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				return result;
+			}
+		}.execute();
+		return null;
+
+	}
+
+	private void uploadFileToStoarge(String appSID, String appKey) {
+		// build URI to upload file in the Aspose cloud storage
+		String strURI = "http://api.aspose.com/v1.1/storage/file/Protection.pptx";
+
+		File file = new File(getFilesDir(), "Protection.pptx");
+		try {
+			SaveStreamToFile(file, getAssets().open("Protection.pptx"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		// Use the following line to use API version 1.1
+		// String strURI = "http://api.aspose.com/v1.1/storage/file/" +
+		// folderName + "/" + fileName;
+
+		String signedURI = Sign(strURI, appSID, appKey);
+
+		String strResponse = UploadFileBinary(file, signedURI, "PUT", mActivity);
+
 	}
 
 	public static String Sign(String UnsignedURL, String AppSID, String AppKey) {
@@ -253,7 +330,7 @@ public class DocumentViewActivity extends Activity {
 
 			// get an hmac_sha1 Mac instance and initialize with the signing key
 			Mac mac = Mac.getInstance("HmacSHA1");
-			//HMAC_SHA1_ALGORITHM
+			// HMAC_SHA1_ALGORITHM
 			mac.init(signingKey);
 
 			// compute the hmac on input data bytes
@@ -273,22 +350,24 @@ public class DocumentViewActivity extends Activity {
 
 	}
 
-//	public static InputStream ProcessCommand(String strURI, String strHttpCommand) throws Exception {
-//
-//
-////		URL address = new URL(strURI);
-////		HttpURLConnection httpCon = (HttpURLConnection) address.openConnection();
-////		httpCon.setDoOutput(true);
-////
-////		httpCon.setRequestProperty("Content-Type", "application/json");
-////		httpCon.setRequestProperty("Accept", "text/json");
-////		httpCon.setRequestMethod(strHttpCommand);
-////		if (strHttpCommand.equals("PUT") || strHttpCommand.equals("POST"))
-////			httpCon.setFixedLengthStreamingMode(0);
-////		String d = httpCon.getResponseMessage();
-////		System.out.println(d);
-////		return httpCon.getInputStream();
-//	}
+	// public static InputStream ProcessCommand(String strURI, String
+	// strHttpCommand) throws Exception {
+	//
+	//
+	// // URL address = new URL(strURI);
+	// // HttpURLConnection httpCon = (HttpURLConnection)
+	// address.openConnection();
+	// // httpCon.setDoOutput(true);
+	// //
+	// // httpCon.setRequestProperty("Content-Type", "application/json");
+	// // httpCon.setRequestProperty("Accept", "text/json");
+	// // httpCon.setRequestMethod(strHttpCommand);
+	// // if (strHttpCommand.equals("PUT") || strHttpCommand.equals("POST"))
+	// // httpCon.setFixedLengthStreamingMode(0);
+	// // String d = httpCon.getResponseMessage();
+	// // System.out.println(d);
+	// // return httpCon.getInputStream();
+	// }
 
 	public void SaveStreamToFile(File outputPath, InputStream inputStream) {
 		OutputStream outputStream = null;
@@ -336,67 +415,52 @@ public class DocumentViewActivity extends Activity {
 		return true;
 	}
 
-}
-
-class GetDocumentFromServer extends AsyncTask<String, Void, InputStream> {
-	
-	File outputPath;
-
-	@Override
-	protected InputStream doInBackground(String... args) {
-		InputStream result = null;
-		String strURI = args[0];
-		String strHttpCommand = args[1];
-		outputPath = new File(args[2]);
-		URL address;
-		try {
-			address = new URL(strURI);
-		
-		HttpURLConnection httpCon = (HttpURLConnection) address.openConnection();
-//		httpCon.setDoOutput(true);
-//
-//		httpCon.setRequestProperty("Content-Type", "application/json");
-//		httpCon.setRequestProperty("Accept", "text/json");
-//		httpCon.setRequestMethod(strHttpCommand);
-//		if (strHttpCommand.equals("PUT") || strHttpCommand.equals("POST"))
-//			httpCon.setFixedLengthStreamingMode(0);
-//		String d = httpCon.getResponseMessage();
-//		System.out.println(d);
-		result = httpCon.getInputStream();
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return result;
+	private boolean isNetworkConnected() {
+		ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo ni = cm.getActiveNetworkInfo();
+		if (ni == null) {
+			// There are no active networks.
+			return false;
+		} else
+			return true;
 	}
-	
-    protected void onPostExecute(InputStream inputStream) {
-        // TODO: check this.exception 
-        // TODO: do something with the feed
-		OutputStream outputStream = null;
-		try {
-			// read this file into InputStream
 
-			// write the inputStream to a FileOutputStream
-			outputStream = new FileOutputStream(outputPath);
+	public static String UploadFileBinary(final File localFile, final String uploadUrl, String strHttpCommand,
+			final Activity mActivity) {
+		new AsyncTask<String, String, String>() {
 
-            int read = 0;
-            byte[] bytes = new byte[8192];
+			@Override
+			protected String doInBackground(String... params) {
+				try {
+					URL url = new URL(uploadUrl);
+					byte[] buf = FileUtils.readFileToByteArray(localFile);
+					HttpURLConnection m_connection;
+					m_connection = (HttpURLConnection) url.openConnection();
+					String parameters = "data=some_post_data";
+					m_connection.setDoOutput(true);
+					m_connection.setRequestMethod("PUT");
+					m_connection.setRequestProperty("Accept", "text/json");
+					m_connection.setRequestProperty("Content-Type", "MultiPart/Form-Data");
+					byte bytes[] = parameters.getBytes();
+					m_connection.setRequestProperty("Content-length", "" + buf.length);
+					m_connection.connect();
+					java.io.OutputStream out = m_connection.getOutputStream();
+					out.write(buf);
+					out.flush();
 
-            while ((read = inputStream.read(bytes)) != -1) {
-               outputStream.write(bytes, 0, read);
-            }
+					InputStream response = m_connection.getInputStream();
+					String res = IOUtils.toString(response);
+					//Toast.makeText(mActivity, res, Toast.LENGTH_SHORT).show();
+					return res;
+				} catch (Exception e) {
+					e.printStackTrace();
+					return null;
+				}
+			}
 
-            inputStream.close();
-            outputStream.flush();
-            outputStream.close();
+		}.execute();
+		return null;
 
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-	
+	}
+
 }
